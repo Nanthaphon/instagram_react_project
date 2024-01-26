edbf68b4390da8fdf90427e322d93f8cd69f7128
unknown <Nanthaphon909@gmail.com>
2024-01-27 02:52:49 +0700

    jujutsu kaisen

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
 function App() {
-  return <div className="text-5xl text-green-500">fuck</div>;
+  return (
+    <div>
+      <div className="w-full bg-orange-600 h-[95px] flex flex-row justify-between items-center">
+        <div className="flex flex-row gap-8 mx-8 font-bold text-white items-center">
+          <div className="w-[100px]">
+            <img
+              src="https://manga168.com/wp-content/uploads/2023/06/168-no1.webp"
+              alt=""
+            />
+          </div>
+          <div>หน้าหลัก</div>
+          <div>ขายดี</div>
+        </div>
+        <div className="bg-gray-950 w-[200px] h-[30px] mx-8  rounded-3xl">
+          <h2 className="text-white text-sm h-full flex items-center mx-5">
+            ค้นหา
+          </h2>
+        </div>
+      </div>
+      <div>
+        <img
+          src="https://media.discordapp.net/attachments/1169121185053818954/1200527991491539054/jujutsu-kaisen-season-2-the-anime-phenomenon-bridging-past-and-present-948083.png?ex=65c681d1&is=65b40cd1&hm=d72161a4987e639e50e311515f849d37c9c66cc9d8181de4439992ad86c1afcc&=&format=webp&quality=lossless&width=1202&height=676"
+          alt=""
+          className="w-full"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
